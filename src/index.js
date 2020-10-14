252c0081326bf59e4bb169ef2abe117d104f592d
Elizaveta Nikolaevich <elizaveta.nikolaevich@l568.local>
2020-10-14 23:37:44 +0300

    feat: reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    let str = Math.abs(n).toString();
+    let newArr = str.split('').reverse()
+    let newSrt = newArr.join('')
+    return parseInt(newSrt)
 }
